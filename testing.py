329ea66e17b5a93b2cd3c6d3581a01febec411e9
danielvillarg <d.villarg@uniandes.edu.co>
2024-05-13 17:06:41 -0500

    cambios

--- testing.py
+++ testing.py
@@ -1,14 +1,35 @@
+
 import gym
+import numpy as np
+import time
+env=gym.make('CartPole-v1',render_mode='human')
 
-env = gym.make("CartPole-v1")
-
-def random_games():
-    for episode in range(10):  # Juega 10 episodios
-        state = env.reset()  # Reinicia el entorno al estado inicial
-        for t in range(500):  # Intenta hasta 500 pasos por episodio
-            env.render()  # Renderiza el entorno visualmente
-            action = env.action_space.sample()  # Elige una acción aleatoria
-            print(env.step(action))  # Imprime el resultado de la acción
-    env.close()  # Cierra el entorno correctamente
-
-random_games()
+(state,_)=env.reset()
+env.render()
+env.step(0)
+env.observation_space
+env.observation_space.high
+env.observation_space.low
+env.action_space
+env.spec
+env.spec.max_episode_steps
+env.spec.reward_threshold
+# simulate the environment
+episodeNumber=5
+timeSteps=100
+ 
+for episodeIndex in range(episodeNumber):
+    initial_state=env.reset()
+    print(episodeIndex)
+    env.render()
+    appendedObservations=[]
+    for timeIndex in range(timeSteps):
+        print(timeIndex)
+        random_action=env.action_space.sample()
+        observation, reward, terminated, truncated, info =env.step(random_action)
+        appendedObservations.append(observation)
+        time.sleep(0.1)
+        if (terminated):
+            time.sleep(1)
+            break
+env.close() 
